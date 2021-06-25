a1dd08f8d939574b7cebc602ba7da566fe7d2e2e
Justin Lam <justin.s.lam@uwaterloo.ca>
2021-06-25 00:30:58 -0400

    Completed Exercise 3.3

--- index.js
+++ index.js
@@ -40,12 +40,24 @@ app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
 app.get('/info', (request, response) => {
     response.send(`
     <p>Phonebook has info for ${persons.length} people.</p>
     <p>${Date()}</p>
     `)
 })
+
 
 const PORT = 3001
 app.listen(PORT, () => {
