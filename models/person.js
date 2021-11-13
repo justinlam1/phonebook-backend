ed0f5aae07f68547841c74a342724f912be944f3
Justin Lam <justin.s.lam@uwaterloo.ca>
2021-11-12 22:12:33 -0500

    Completed Exercise 3.20

--- models/person.js
+++ models/person.js
@@ -16,11 +16,13 @@ mongoose.connect(url)
 const personSchema = new mongoose.Schema({
     name: {
         type: String,
+        minLength: 3,
         required: true,
         unique: true
     },
     number: {
         type: String,
+        minLength: 8,
         required: true,
     }
 })
